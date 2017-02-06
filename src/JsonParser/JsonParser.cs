799f5d5930ad2dc981dbaac16a814ced343f64ef
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2017-02-06 18:02:57 +0100

    Updated nuget.config format, removed mixed tab-spaces

--- src/JsonParser/JsonParser.cs
+++ src/JsonParser/JsonParser.cs
@@ -8,104 +8,96 @@ namespace JsonParser
 {
     public static class JsonParser
     {
-	    public static object Parse(string json)
-	    {
-		    return Parse(new StringReader(json));
-	    }
+        public static object Parse(string json)
+        {
+            return Parse(new StringReader(json));
+        }
 
         public static object Parse(TextReader en)
         {
             en.EatWhitespaces();
             var ch = en.PeekOrFail();
-			if (ch == '{')
+            if (ch == '{')
             {
-	            en.Read();
+                en.Read();
                 var dict = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
                 en.EatWhitespaces();
                 while (en.PeekOrFail() != '}')
                 {
                     var key = ParseString(en);
                     en.EatWhitespaces();
-					en.ReadExpected(":");
+                    en.ReadExpected(":");
                     dict.Add(key, Parse(en));
                     en.EatWhitespaces();
                     if (en.PeekOrFail() == ',')
                     {
-	                    en.Read();
-						en.EatWhitespaces();
+                        en.Read();
+                        en.EatWhitespaces();
                     }
                 }
-				en.Read();
+                en.Read();
                 return dict;
             }
             if (ch == '[')
             {
-	            en.Read();
+                en.Read();
                 var list = new List<object>();
                 en.EatWhitespaces();
-				while (en.PeekOrFail() != ']')
+                while (en.PeekOrFail() != ']')
                 {
                     list.Add(Parse(en));
                     en.EatWhitespaces();
                     if (en.PeekOrFail() == ',')
                     {
-	                    en.Read();
-						en.EatWhitespaces();
+                        en.Read();
+                        en.EatWhitespaces();
                     }
                 }
-	            en.Read();
+                en.Read();
                 return list;
             }
             if (ch == 'n')
             {
-				en.ReadExpected("null");
+                en.ReadExpected("null");
                 return null;
             }
             if (ch == 't')
             {
-				en.ReadExpected("true");
-				return true;
+                en.ReadExpected("true");
+                return true;
             }
             if (ch == 'f')
             {
-				en.ReadExpected("false");
-				return false;
+                en.ReadExpected("false");
+                return false;
             }
             if (ch == '"')
-            {
                 return ParseString(en);
-            }
             if (ch == '-' || char.IsDigit(ch))
             {
                 var sb = new StringBuilder();
-				while (ch == '+' || ch == '.' || ch == '-' || ch == 'e' || ch == 'E' || char.IsDigit(ch))
+                while (ch == '+' || ch == '.' || ch == '-' || ch == 'e' || ch == 'E' || char.IsDigit(ch))
                 {
                     sb.Append(en.ReadOrFail());
                     var peek = en.Peek();
                     if (peek == -1)
-                    {
                         break;
-                    }
                     ch = (char) peek;
                 }
                 decimal result;
                 if (decimal.TryParse(sb.ToString(), NumberStyles.Float, CultureInfo.InvariantCulture, out result))
-                {
                     return result;
-                }
-				throw new FormatException(string.Format("Unable to parse '{0}' as number", sb));
+                throw new FormatException(string.Format("Unable to parse '{0}' as number", sb));
             }
             throw new FormatException(string.Format("Unexpected char '{0}'", ch));
         }
 
         private static string ParseString(TextReader en)
         {
-			en.EatWhitespaces();
-	        var ch = en.ReadOrFail();
+            en.EatWhitespaces();
+            var ch = en.ReadOrFail();
             if (ch != '"')
-            {
                 throw new FormatException(string.Format("Expected '\"', found '{0}'", ch));
-            }
             var sb = new StringBuilder();
             while (true)
             {
@@ -152,46 +144,40 @@ namespace JsonParser
             }
         }
 
-		public static void ReadExpected(this TextReader tr, string str)
-		{
-			foreach (var expected in str)
-			{
-				var actual = tr.ReadOrFail();
-				if (actual != expected)
-				{
-					throw new FormatException(string.Format("Expected '{0}', found '{1}'", expected, actual));
-				}
-			}
-		}
+        public static void ReadExpected(this TextReader tr, string str)
+        {
+            foreach (var expected in str)
+            {
+                var actual = tr.ReadOrFail();
+                if (actual != expected)
+                    throw new FormatException(string.Format("Expected '{0}', found '{1}'", expected, actual));
+            }
+        }
 
-	    private static char ReadOrFail(this TextReader tr)
-		{
-			var read = tr.Read();
-			if (read == -1)
-			{
-				throw new FormatException("Unexpected end of stream");
-			}
-			return (char)read;
-		}
+        private static char ReadOrFail(this TextReader tr)
+        {
+            var read = tr.Read();
+            if (read == -1)
+                throw new FormatException("Unexpected end of stream");
+            return (char) read;
+        }
 
-	    private static char PeekOrFail(this TextReader tr)
-		{
-			var peek = tr.Peek();
-			if (peek == -1)
-			{
-				throw new FormatException("Unexpected end of stream");
-			}
-			return (char)peek;
-		}
+        private static char PeekOrFail(this TextReader tr)
+        {
+            var peek = tr.Peek();
+            if (peek == -1)
+                throw new FormatException("Unexpected end of stream");
+            return (char) peek;
+        }
 
-	    private static void EatWhitespaces(this TextReader tr)
-		{
-			var next = tr.Peek();
-			while (next != -1 && char.IsWhiteSpace((char)next))
-			{
-				tr.Read();
-				next = tr.Peek();
-			}
-		}
-	}
+        private static void EatWhitespaces(this TextReader tr)
+        {
+            var next = tr.Peek();
+            while (next != -1 && char.IsWhiteSpace((char) next))
+            {
+                tr.Read();
+                next = tr.Peek();
+            }
+        }
+    }
 }
--- src/JsonParser/JsonParserTest.cs
+++ src/JsonParser/JsonParserTest.cs
@@ -4,42 +4,42 @@ namespace JsonParser
 {
     public class JsonParserTest
     {
-	    [Test]
-	    public void Should_parse_numbers()
-	    {
-			Assert.That(JsonParser.Parse("1"), Is.EqualTo(1));
-			Assert.That(JsonParser.Parse("  1  "), Is.EqualTo(1));
-			Assert.That(JsonParser.Parse("3.14"), Is.EqualTo(3.14));
-			Assert.That(JsonParser.Parse("3.14"), Is.EqualTo(3.14));
-			Assert.That(JsonParser.Parse("-1"), Is.EqualTo(-1));
-	    }
+        [Test]
+        public void Should_parse_numbers()
+        {
+            Assert.That(JsonParser.Parse("1"), Is.EqualTo(1));
+            Assert.That(JsonParser.Parse("  1  "), Is.EqualTo(1));
+            Assert.That(JsonParser.Parse("3.14"), Is.EqualTo(3.14));
+            Assert.That(JsonParser.Parse("3.14"), Is.EqualTo(3.14));
+            Assert.That(JsonParser.Parse("-1"), Is.EqualTo(-1));
+        }
 
-	    [Test]
-	    public void Should_parse_strings()
-	    {
-			Assert.That(JsonParser.Parse("  \" a b c \"  "), Is.EqualTo(" a b c "));
-	    }
+        [Test]
+        public void Should_parse_strings()
+        {
+            Assert.That(JsonParser.Parse("  \" a b c \"  "), Is.EqualTo(" a b c "));
+        }
 
-	    [Test]
-	    public void Should_parse_empty_obj()
-	    {
-			Assert.That(JsonParser.Parse("{}"), Has.Count.EqualTo(0));
-			Assert.That(JsonParser.Parse("  {   }  "), Has.Count.EqualTo(0));
-			Assert.That(JsonParser.Parse("  { \"a\"  : {   }  }  "), Has.Count.EqualTo(1));
-	    }
+        [Test]
+        public void Should_parse_empty_obj()
+        {
+            Assert.That(JsonParser.Parse("{}"), Has.Count.EqualTo(0));
+            Assert.That(JsonParser.Parse("  {   }  "), Has.Count.EqualTo(0));
+            Assert.That(JsonParser.Parse("  { \"a\"  : {   }  }  "), Has.Count.EqualTo(1));
+        }
 
-	    [Test]
-	    public void Should_parse_empty_ary()
-	    {
-			Assert.That(JsonParser.Parse("[]"), Has.Count.EqualTo(0));
-			Assert.That(JsonParser.Parse("  [   ]  "), Has.Count.EqualTo(0));
-			Assert.That(JsonParser.Parse("  [ [ ] , [] , [   ]   ]  "), Has.Count.EqualTo(3));
-	    }
+        [Test]
+        public void Should_parse_empty_ary()
+        {
+            Assert.That(JsonParser.Parse("[]"), Has.Count.EqualTo(0));
+            Assert.That(JsonParser.Parse("  [   ]  "), Has.Count.EqualTo(0));
+            Assert.That(JsonParser.Parse("  [ [ ] , [] , [   ]   ]  "), Has.Count.EqualTo(3));
+        }
 
-		[Test]
-		public void Should_parse_a_complex_json()
-		{
-			var json = @"{
+        [Test]
+        public void Should_parse_a_complex_json()
+        {
+            var json = @"{
                 'strProp': 'val',
                 'numProp': -3.14e2,
                 'trueProp': true,
@@ -66,46 +66,46 @@ namespace JsonParser
 					'emptyObj': {}
 				}
             }".Replace('\'', '"');
-			dynamic o = JsonParser.Parse(json);
+            dynamic o = JsonParser.Parse(json);
 
-			Assert.That(o, Has.Count.EqualTo(9));
-			Assert.That(o["strProp"], Is.EqualTo("val"));
-			Assert.That(o["numProp"], Is.EqualTo(-314));
-			Assert.That(o["trueProp"], Is.True);
-			Assert.That(o["falseProp"], Is.False);
-			Assert.That(o["nullProp"], Is.Null);
-			Assert.That(o["emptyAry"], Has.Count.EqualTo(0));
-			Assert.That(o["emptyObj"], Has.Count.EqualTo(0));
-			Assert.That(o["aryProp"], Has.Count.EqualTo(7));
-			Assert.That(o["aryProp"][0], Is.EqualTo("val"));
-			Assert.That(o["aryProp"][1], Is.EqualTo(-314));
-			Assert.That(o["aryProp"][2], Is.True);
-			Assert.That(o["aryProp"][3], Is.False);
-			Assert.That(o["aryProp"][4], Is.Null);
-			Assert.That(o["aryProp"][5], Has.Count.EqualTo(0));
-			Assert.That(o["aryProp"][6], Has.Count.EqualTo(0));
-			Assert.That(o["objProp"], Has.Count.EqualTo(7));
-			Assert.That(o["objProp"]["strProp"], Is.EqualTo("val"));
-			Assert.That(o["objProp"]["numProp"], Is.EqualTo(-314));
-			Assert.That(o["objProp"]["trueProp"], Is.True);
-			Assert.That(o["objProp"]["falseProp"], Is.False);
-			Assert.That(o["objProp"]["nullProp"], Is.Null);
-			Assert.That(o["objProp"]["emptyAry"], Has.Count.EqualTo(0));
-			Assert.That(o["objProp"]["emptyObj"], Has.Count.EqualTo(0));
-		}
+            Assert.That(o, Has.Count.EqualTo(9));
+            Assert.That(o["strProp"], Is.EqualTo("val"));
+            Assert.That(o["numProp"], Is.EqualTo(-314));
+            Assert.That(o["trueProp"], Is.True);
+            Assert.That(o["falseProp"], Is.False);
+            Assert.That(o["nullProp"], Is.Null);
+            Assert.That(o["emptyAry"], Has.Count.EqualTo(0));
+            Assert.That(o["emptyObj"], Has.Count.EqualTo(0));
+            Assert.That(o["aryProp"], Has.Count.EqualTo(7));
+            Assert.That(o["aryProp"][0], Is.EqualTo("val"));
+            Assert.That(o["aryProp"][1], Is.EqualTo(-314));
+            Assert.That(o["aryProp"][2], Is.True);
+            Assert.That(o["aryProp"][3], Is.False);
+            Assert.That(o["aryProp"][4], Is.Null);
+            Assert.That(o["aryProp"][5], Has.Count.EqualTo(0));
+            Assert.That(o["aryProp"][6], Has.Count.EqualTo(0));
+            Assert.That(o["objProp"], Has.Count.EqualTo(7));
+            Assert.That(o["objProp"]["strProp"], Is.EqualTo("val"));
+            Assert.That(o["objProp"]["numProp"], Is.EqualTo(-314));
+            Assert.That(o["objProp"]["trueProp"], Is.True);
+            Assert.That(o["objProp"]["falseProp"], Is.False);
+            Assert.That(o["objProp"]["nullProp"], Is.Null);
+            Assert.That(o["objProp"]["emptyAry"], Has.Count.EqualTo(0));
+            Assert.That(o["objProp"]["emptyObj"], Has.Count.EqualTo(0));
+        }
 
-	        [Test]
-	        public void Should_parse_escaped_chars()
-	        {
-	            Assert.That(JsonParser.Parse("\"\\a\""), Is.EqualTo("\a"));
-	            Assert.That(JsonParser.Parse("\"\\b\""), Is.EqualTo("\b"));
-	            Assert.That(JsonParser.Parse("\"\\f\""), Is.EqualTo("\f"));
-	            Assert.That(JsonParser.Parse("\"\\r\""), Is.EqualTo("\r"));
-	            Assert.That(JsonParser.Parse("\"\\n\""), Is.EqualTo("\n"));
-	            Assert.That(JsonParser.Parse("\"\\t\""), Is.EqualTo("\t"));
-	            Assert.That(JsonParser.Parse("\"\\v\""), Is.EqualTo("\v"));
-	            Assert.That(JsonParser.Parse("\"\\\"\""), Is.EqualTo("\""));
-	            Assert.That(JsonParser.Parse("\"\\\\\""), Is.EqualTo("\\"));
-	        }
-	}
+        [Test]
+        public void Should_parse_escaped_chars()
+        {
+            Assert.That(JsonParser.Parse("\"\\a\""), Is.EqualTo("\a"));
+            Assert.That(JsonParser.Parse("\"\\b\""), Is.EqualTo("\b"));
+            Assert.That(JsonParser.Parse("\"\\f\""), Is.EqualTo("\f"));
+            Assert.That(JsonParser.Parse("\"\\r\""), Is.EqualTo("\r"));
+            Assert.That(JsonParser.Parse("\"\\n\""), Is.EqualTo("\n"));
+            Assert.That(JsonParser.Parse("\"\\t\""), Is.EqualTo("\t"));
+            Assert.That(JsonParser.Parse("\"\\v\""), Is.EqualTo("\v"));
+            Assert.That(JsonParser.Parse("\"\\\"\""), Is.EqualTo("\""));
+            Assert.That(JsonParser.Parse("\"\\\\\""), Is.EqualTo("\\"));
+        }
+    }
 }
